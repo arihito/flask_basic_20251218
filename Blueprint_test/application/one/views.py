4cba56c8a059cd6a253dbd202951f3ea35b7273a
kenshu <kenshu.app@gmail.com>
2026-01-05 13:02:59 +0900

    move: test directory

--- Blueprint_test/application/one/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-one_bp = Blueprint('one_app', __name__, url_prefix='/one')
-
-@one_bp.route('/')
-def show_template():
-  return render_template('one/index.j2')
